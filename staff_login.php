dde72af2751416f9a36b11f97fddbb77c7839892
Tuitchi <154252905+Tuitchi@users.noreply.github.com>
2025-07-02 18:11:07 +0800

    Update staff_login.php

--- staff_login.php
+++ staff_login.php
@@ -197,8 +197,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <!-- Right: Form -->
                 <div class="md:w-1/2 p-8 flex flex-col justify-center">
                     <?php if (isset($error)): ?>
-                        <div class="bg-green-100 text-green-800 border-l-4 border-green-400 rounded p-3 mb-6 flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-green-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12A9 9 0 113 12a9 9 0 0118 0z" /></svg>
+                        <div class="bg-red-100 text-red-800 border-l-4 border-red-400 rounded p-3 mb-6 flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12A9 9 0 113 12a9 9 0 0118 0z" /></svg>
                             <span><?php echo $error; ?></span>
                         </div>
                     <?php endif; ?>
